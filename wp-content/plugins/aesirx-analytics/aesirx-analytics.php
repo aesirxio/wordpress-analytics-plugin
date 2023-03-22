85b0e4675521f8c1042b27272b3bf94cea2b0832
Tito Alvarez <augustoalvarez@gmail.com>
2023-03-21 22:05:18 -0600

    gitignore and v.1.0.0 release

--- wp-content/plugins/aesirx-analytics/aesirx-analytics.php
+++ wp-content/plugins/aesirx-analytics/aesirx-analytics.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: aesirx-analytics
  * Description: Aesirx analytics plugin.
- * Version: 0.1
+ * Version: 1.0.0
  * Author: aesirx.io
  * Author URI: https://aesirx.io/
  * Domain Path: /languages
