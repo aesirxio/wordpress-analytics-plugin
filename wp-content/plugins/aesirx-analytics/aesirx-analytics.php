c4c3951404da169fcf48de8f48ccff64ecb20b2d
Tito Alvarez <augustoalvarez@gmail.com>
2023-03-21 22:14:28 -0600

    Bump v.1.0.1 - alpha

--- wp-content/plugins/aesirx-analytics/aesirx-analytics.php
+++ wp-content/plugins/aesirx-analytics/aesirx-analytics.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: aesirx-analytics
  * Description: Aesirx analytics plugin.
- * Version: 1.0.0
+ * Version: 1.0.1-alpha.1
  * Author: aesirx.io
  * Author URI: https://aesirx.io/
  * Domain Path: /languages
