efacbbe1ba5bc9960632065fbfce9e0eceb819c9
Lu Nguyen <lu@r-digital.tech>
2023-04-25 13:01:38 +0700

    BI view

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,59 @@
-const zip = require("gulp-zip");
-const gulp = require("gulp");
-const composer = require("gulp-composer")
-
+const zip = require('gulp-zip');
+const gulp = require('gulp');
+const composer = require('gulp-composer');
+const webpack = require('webpack-stream');
 
 async function cleanTask() {
-  const del = await import("del");
-  return del.deleteAsync("./dist/plugin/**", { force: true });
+  const del = await import('del');
+  return del.deleteAsync('./dist/plugin/**', { force: true });
 }
 
 function movePluginFolderTask() {
   return gulp
-    .src(["./wp-content/plugins/aesirx-analytics/**"])
-    .pipe(gulp.dest("./dist/plugin/aesirx-analytics"));
+    .src(['./wp-content/plugins/aesirx-analytics/**'])
+    .pipe(gulp.dest('./dist/plugin/aesirx-analytics'));
 }
 
 function moveAnalyticJSTask() {
   return gulp
-    .src(["./node_modules/aesirx-analytics/dist/analytics.js"])
-    .pipe(gulp.dest("./dist/plugin/aesirx-analytics/assets/js"));
+    .src(['./node_modules/aesirx-analytics/dist/analytics.js'])
+    .pipe(gulp.dest('./dist/plugin/aesirx-analytics/assets/js'));
+}
+
+function webpackBIApp() {
+  return gulp
+    .src('./assets/bi/index.tsx')
+    .pipe(webpack(require('./webpack.config.js')))
+    .pipe(gulp.dest('./dist/plugin/aesirx-analytics'));
 }
 
 function compressTask() {
   return gulp
-    .src("./dist/plugin/**")
-    .pipe(zip("plg_aesirx_analytics.zip"))
-    .pipe(gulp.dest("./dist"));
+    .src('./dist/plugin/**')
+    .pipe(zip('plg_aesirx_analytics.zip'))
+    .pipe(gulp.dest('./dist'));
 }
 
 function composerTask() {
-    return composer({
-        "working-dir": "./dist/plugin/aesirx-analytics"
-    })
+  return composer({
+    'working-dir': './dist/plugin/aesirx-analytics',
+  });
 }
 
 async function cleanComposerTask() {
-    const del = await import("del")
-    return del.deleteAsync('./dist/plugin/aesirx-analytics/composer.*', {force:true});
+  const del = await import('del');
+  return del.deleteAsync('./dist/plugin/aesirx-analytics/composer.*', {
+    force: true,
+  });
 }
 
 exports.zip = gulp.series(
   cleanTask,
   movePluginFolderTask,
   moveAnalyticJSTask,
+  webpackBIApp,
   composerTask,
-  cleanComposerTask,
-  compressTask,
-  cleanTask
+  //  cleanComposerTask,
+  compressTask
+  //cleanTask
 );
---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ProvidePlugin } = require('webpack');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'image/[contenthash][ext][query]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[contenthash][ext][query]',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      filename: 'includes/settings.php',
+      template: './wp-content/plugins/aesirx-analytics/includes/settings.php',
+      minify: false,
+    }),
+
+    new FileManagerPlugin({
+      events: {
+        onEnd: {
+          copy: [
+            {
+              source: path.resolve(__dirname, './node_modules/aesirx-bi-app/public/assets/images/'),
+              destination: path.resolve(__dirname, './dist/plugin/aesirx-analytics/assets/images/'),
+            },
+            {
+              source: path.resolve(__dirname, './node_modules/aesirx-bi-app/public/assets/data/'),
+              destination: path.resolve(__dirname, './dist/plugin/aesirx-analytics/assets/data/'),
+            },
+          ],
+        },
+      },
+    }),
+  ],
+
+  output: {
+    filename: 'assets/bi/js/[contenthash].js',
+    publicPath: '/wp-content/plugins/aesirx-analytics/',
+    clean: true,
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  resolve: {
+    alias: {
+      react$: require.resolve(path.resolve(__dirname, './node_modules/react')),
+    },
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
